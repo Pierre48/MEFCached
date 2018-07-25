a1f65ebf57bcc98d8503a07dbfcb8538d2446121
pierre.plagnes@gmail.com <pierre.plagnes@gmail.com>
2018-07-25 10:11:59 +0200

    initial version

---
+++ src/GitCached/BootStrapClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ColorConsole;
+
+namespace GitCached
+{
+    internal class BootStrapClass
+    {
+        private bool _started = false;
+
+        internal void Start()
+        {
+            if (_started)
+            {
+                CConsole.WriteLine("Bootstrap is already started !",ConsoleColor.Red);
+                return;
+            }
+            CConsole.WriteLine("Starting ... ");
+            CConsole.WriteLine("Started", ConsoleColor.Green);
+            _started = true;
+        }
+        internal void Stop()
+        {
+
+            if (!_started)
+            {
+                CConsole.WriteLine("Bootstrap is not started !", ConsoleColor.Red);
+                return;
+            }
+        }
+    }
+}
---
+++ src/GitCached/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCached
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bootStrap = new BootStrapClass();
+            bootStrap.Start();
+            //var catalog = new AggregateCatalog();
+
+            ////Add all the parts found in all assemblies in
+            ////the same directory as the executing program
+            //catalog.Catalogs.Add(
+            //    new DirectoryCatalog(
+            //        Path.GetDirectoryName(
+            //        Assembly.GetExecutingAssembly().Location
+            //        )
+            //    )
+            //);
+
+            ////Create the CompositionContainer with the parts in the catalog.
+            //CompositionContainer container = new CompositionContainer(catalog);
+
+            ////Fill the imports of this object
+            //container.ComposeParts(this);
+        }
+    }
+}
